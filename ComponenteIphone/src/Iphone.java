8883fef2e3b81fbbae17b76a5f936aaec16ceed3
ArielGon7 <arielgomessantos7@gmail.com>
2025-04-29 23:01:19 -0300

    Add files via upload

---
+++ ComponenteIphone/src/Iphone.java
@@ -0,0 +1,46 @@
+public class Iphone implements ReprodutorMusical, AparelhoEletronico, NavegadorNaInternet{
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para o numero " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo pagina url :" + url );
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando uma nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando pagina");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando musica ");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando musica");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando musica " + musica);
+    }
+}
---
+++ ComponenteIphone/src/Usuario.java
@@ -0,0 +1,23 @@
+public class Usuario {
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone();
+
+        iphone1.tocar();
+        iphone1.pausar();
+        iphone1.selecionarMusica("The Smiths");
+
+        iphone1.ligar("555-111");
+        iphone1.atender();
+        iphone1.iniciarCorreioVoz();
+
+        iphone1.exibirPagina("https://github.com/ArielGon7");
+        iphone1.adicionarNovaAba();
+        iphone1.atualizarPagina();
+
+
+
+
+
+
+    }
+}
